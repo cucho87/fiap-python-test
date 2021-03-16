5ef3fa8943bbde539d2d3dc2a645832f14332a69
cucho87 <astevensonsolar@gmail.com>
2021-03-15 21:12:11 -0300

    teste 3

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1>Hola FIAP!</h1>\nMBA 2! o/"
+    return "<h1>Hola FIAP!</h1>\nMBA 3! o/"
 
 if __name__ == '__main__':
     # application.run()
